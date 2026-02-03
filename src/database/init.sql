1eb64aac48db7008b604579c42dfbf6b1906143a
AdriaMontero <monterosanchezadria@gmail.com>
2026-02-03 16:26:49 +0100

    subidasql

--- src/database/init.sql
+++ src/database/init.sql
@@ -3,7 +3,7 @@ USE extagram_db;
 
 CREATE TABLE IF NOT EXISTS posts (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    post TEXT NOT NULL,
+    post TEXT NULL,
     photourl VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     INDEX idx_created (created_at)
